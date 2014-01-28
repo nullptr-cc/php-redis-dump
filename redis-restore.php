66f30e8fbbedf2d4d6b244ab999df853c2d04cc9
Eugene Fidelin <eugef@yandex.ru>
2014-01-28 16:54:40 +0100

    Improve: Add 3 modes for restoring dump - slush, replace and insert

--- redis-restore.php
+++ redis-restore.php
@@ -4,12 +4,13 @@ if (!extension_loaded('redis')) {
     die("redis extension not loaded\n");
 };
 
-$opt = getopt('h:p:s:n:');
+$opt = getopt('h:p:s:n:m:');
 
 $host = isset($opt['h']) ? $opt['h'] : '127.0.0.1';
 $port = isset($opt['p']) ? $opt['p'] : 6379;
 $socket = isset($opt['s']) ? $opt['s'] : null;
 $dbnum = isset($opt['n']) ? $opt['n'] : 0;
+$mode = (isset($opt['m']) && in_array($opt['m'], array('flush', 'replace', 'insert'))) ? $opt['m'] : 'replace';
 
 $redis = new Redis();
 
@@ -22,9 +23,15 @@ try {
 
 $input = fopen('php://stdin', 'r');
 
+if ($mode == 'flush') {
+    $redis->flushDB();
+}    
+
 while($line = stream_get_line($input, 65535, "\n")) {
     sscanf($line, "%s %d %s\n", $key, $ttl, $value);
-    $redis->del($key);
+    if ($mode == 'replace') {
+        $redis->del($key);
+    }    
     $redis->restore($key, $ttl, pack("H*" , $value));
 };
 
